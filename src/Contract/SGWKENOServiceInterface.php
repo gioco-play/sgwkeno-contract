32058e8768ac93e2d596681b8d59ada04039f35c
Amin56413 <Ellie.guo@goldenf.co>
2023-11-24 11:24:47 +0800

    update

--- src/Contract/SGWKENOServiceInterface.php
+++ src/Contract/SGWKENOServiceInterface.php
@@ -12,7 +12,9 @@ interface SGWKENOServiceInterface {
      * @param string $account
      * @param string $gameCode
      * @param string $language
+     * @param string $vendorCode
      * @param string $backUrl
+     * @param string $oddType
      * @return mixed
      */
     function gameLaunch(string $opCode, string $account, string $gameCode, string $language, string $vendorCode, string $backUrl = '', string $oddType = '');
