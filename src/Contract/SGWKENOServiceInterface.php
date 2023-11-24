17c6b309c4d75ba8ade3df0ffe50d78b8d1b6bc6
Amin56413 <Ellie.guo@goldenf.co>
2023-11-24 16:13:35 +0800

    update

--- src/Contract/SGWKENOServiceInterface.php
+++ src/Contract/SGWKENOServiceInterface.php
@@ -1,8 +1,9 @@
 <?php
 declare(strict_types=1);
 namespace GiocoPlus\SGWKENO\Contract;
+
 /**
- * rsg
+ * sgwkeno
  */
 interface SGWKENOServiceInterface {
 
@@ -24,63 +25,66 @@ interface SGWKENOServiceInterface {
      * @param string $opCode
      * @param string $account
      * @param string $betId
+     * @param string $vendorCode
      * @param string $language
      * @return mixed
      */
-    function gameDetail(string $opCode, string $account, string $betId, string $language = 'en');
+    function gameDetail(string $opCode, string $account, string $betId, string $vendorCode, string $language = 'en');
 
     /**
      * 抓取遊戲紀錄
      * @param string $opCode
      * @param int $pastMinutes
      * @param string $cacheKey
+     * @param string $vendorCode
      * @return mixed
      */
-    function betLogGrabber(string $opCode, int $pastMinutes, string $cacheKey);
+    function betLogGrabber(string $opCode, int $pastMinutes, string $cacheKey, string $vendorCode);
 
     /**
      * 全營商 抓取遊戲紀錄
-     *
      * @param int $pastMinutes
+     * @param string $vendorCode
      * @return mixed
      */
-    function betLogGrabberAll(int $pastMinutes);
+    function betLogGrabberAll(int $pastMinutes, string $vendorCode);
 
     /**
      * 遊戲上分
-     *
      * @param string $opCode
      * @param string $account
+     * @param string $vendorCode
      * @return mixed
      */
-    function gameTransferIn(string $opCode, string $account);
+    function gameTransferIn(string $opCode, string $account, string $vendorCode);
 
     /**
      * 遊戲下分
-     *
      * @param string $opCode
      * @param string $account
      * @param float $amount
+     * @param string $vendorCode
      * @return mixed
      */
-    function gameTransferOut(string $opCode, string $account, float $amount);
+    function gameTransferOut(string $opCode, string $account, float $amount, string $vendorCode);
 
     /**
      * 取得錢包餘額
-     *
      * @param string $opCode
      * @param string $account
+     * @param string $vendorCode
      * @return mixed
      */
-    function getBalance(string $opCode, string $account);
+    function getBalance(string $opCode, string $account, string $vendorCode);
 
     /**
      * 上/下分失敗 訂單檢核
      * @param string $opCode
      * @param string $orderNo
+     * @param string $vendorCode
      * @return mixed
      */
-    function orderFailCheck(string $opCode, string $orderNo);
+    function orderFailCheck(string $opCode, string $orderNo, string $vendorCode);
 
     /**
      * 營商帳號轉換為遊戲商帳號
